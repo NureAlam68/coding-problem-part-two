6f29ada5d39d9b8c94685727d255c5cbaa90e23d
NureAlam68 <nurealam151068@gmail.com>
2024-09-08 15:59:16 +0600

    quantity js

---
+++ quantity.js
@@ -0,0 +1,17 @@
+//  wood quantity 2 or more table , chair, bed
+
+
+function woodQuantity(chairQuantity, tableQuantity, bedQuantity) {
+    const perChairWood = 3;
+    const perTableWood = 10;
+    const perBedWood = 30;
+
+    const chairTotalWood = chairQuantity * perChairWood;
+    const tableTotalWood = tableQuantity * perTableWood;
+    const bedTotalWood = bedQuantity * perBedWood;
+
+    const totalWood = chairTotalWood + tableTotalWood + bedTotalWood;
+    return totalWood;
+}
+const result = woodQuantity(2, 1, 1);
+console.log('Total wood need:', result, 'cft.');
