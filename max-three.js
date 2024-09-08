078b1dec7665499fd8054df9589d918636b08792
NureAlam68 <nurealam151068@gmail.com>
2024-09-08 15:31:21 +0600

    max number between some numbers

--- max-three.js
+++ max-three.js
@@ -1,16 +1,27 @@
 // max number between three number
 
 
-function maxOfThree(num1, num2, num3) {
-    if(num1 > num2 && num1 > num2) {
-        return num1;
-    }
-    else if(num2 > num1 && num2 > num3) {
-        return num2;
-    }
-    else {
-        return num3;
-    }
-}
-let max = maxOfThree(200, 400, 66);
-console.log('max of three:', max);
+// function maxOfThree(num1, num2, num3) {
+//     if(num1 > num2 && num1 > num2) {
+//         return num1;
+//     }
+//     else if(num2 > num1 && num2 > num3) {
+//         return num2;
+//     }
+//     else {
+//         return num3;
+//     }
+// }
+// let max = maxOfThree(200, 400, 66);
+// console.log('max of three:', max);
+
+
+
+
+// max number between some number 
+
+
+const max = Math.max(22, 55,33, 9, 90, 555, 2748, 593,21, 68);
+console.log('max number is:', max);
+
+
