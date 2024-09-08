ed47804602a57b161f2d368858f022e07cbc21ed
NureAlam68 <nurealam151068@gmail.com>
2024-09-08 17:26:36 +0600

    more product quantity sum

--- array-object-sum.js
+++ array-object-sum.js
@@ -1,15 +1,43 @@
-// 1 product sum array - object
+// 1. product quantity 1: sum array - object
+
+
+
+
+// const products = [
+//     {name: 'Shampoo', price: 200, color: 'black'},
+//     {name: 'Shirt', price: 500, color: 'black'},
+//     {name: 'Pant', price: 1000, color: 'black'},
+//     {name: 'Face-wash', price: 600, color: 'black'},
+// ]
+// function getShoppingTotal(products) {
+//     let total = 0;
+//     for(const product of products) {
+//         total = total + product.price;
+//     }
+//     return total;
+// }
+// let result = getShoppingTotal(products);
+// console.log('Total shopping cost:', result, 'taka.');
+
+
+
+
+
+// more product quantity: sum array - object
+
+
 
 const products = [
-    {name: 'Shampoo', price: 200, color: 'black'},
-    {name: 'Shirt', price: 500, color: 'black'},
-    {name: 'Pant', price: 1000, color: 'black'},
-    {name: 'Face-wash', price: 600, color: 'black'},
+    {name: 'Shampoo', price: 200, color: 'black', quantity: 1},
+    {name: 'Shirt', price: 500, color: 'black', quantity: 2},
+    {name: 'Pant', price: 1000, color: 'black', quantity: 1},
+    {name: 'Face-wash', price: 600, color: 'black', quantity: 3},
 ]
 function getShoppingTotal(products) {
     let total = 0;
     for(const product of products) {
-        total = total + product.price;
+        const thisProductCost = product.price * product.quantity;
+        total = total + thisProductCost;
     }
     return total;
 }
