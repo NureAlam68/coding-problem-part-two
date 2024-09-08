fda0f05c2f9c643ae00176f9829dc3abceee3352
NureAlam68 <nurealam151068@gmail.com>
2024-09-08 18:01:44 +0600

    layered discount

--- discount.js
+++ discount.js
@@ -4,19 +4,57 @@
 
 
 
-function discountedPrice(quantity) {
+// function discountedPrice(quantity) {
+//     if(quantity <= 100) {
+//         const total = quantity * 100;
+//         return total;
+//     }
+//     else if(quantity <= 200) {
+//         const total = quantity * 90;
+//         return total;
+//     }
+//     else {
+//         const total = quantity * 70;
+//         return total;
+//     }
+// }
+// const result = discountedPrice(210);
+// console.log('discounted price:', result, 'taka.');
+
+
+
+
+
+// layered discount 
+
+// first 100 product: 100 taka, 101 to 200 product: 90 taka, above 200: 70 taka
+
+
+
+
+function layeredDiscountedTotal(quantity) {
+    const first100Price = 100;
+    const second100Price = 90;
+    const above200Price = 70;
     if(quantity <= 100) {
-        const total = quantity * 100;
+        const total = quantity * first100Price;
         return total;
     }
     else if(quantity <= 200) {
-        const total = quantity * 90;
+        const first100Total = 100 * first100Price;
+        const remainingQuantity = quantity - 100;
+        const remainingTotal = remainingQuantity * second100Price;
+        const total = first100Total + remainingTotal;
         return total;
     }
     else {
-        const total = quantity * 70;
+        const first100Total = 100 * first100Price;
+        const second100Total = 100 * second100Price;
+        const remainingQuantity = quantity - 200;
+        const remainingTotal = remainingQuantity * above200Price;
+        const total = first100Total + second100Total + remainingTotal;
         return total;
     }
 }
-const result = discountedPrice(210);
-console.log(result);
+const result = layeredDiscountedTotal(250);
+console.log('Discounted price:', result, 'taka.');
